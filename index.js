64707702b39ea7b516c9c9e9a009266a526c6005
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-04-14 16:42:25 +1200

    added

---
+++ index.js
@@ -0,0 +1,24 @@
+// Create an XOR gate function
+
+function XORGate(a, b) {
+  if (typeof a === "boolean" && typeof b === "boolean") {
+    return (a && !b) || (!a && b);
+  }
+}
+
+console.log(XORGate(true,false));
+
+A = [0,0,0,0,1,1,1,1]
+B = [0,0,1,1,0,0,1,1]
+C = [0,1,0,1,0,1,0,1]
+
+function truthTable(a,b,c) {
+    if (typeof a === "boolean" && typeof b === "boolean" && typeof c === "boolean") {
+        columns = function.length();
+        return columns
+
+    }
+}
+
+console.log(truthTable(A,B,C));
+
